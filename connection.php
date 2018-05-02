340e3a1ba2b3cd33b88172234c3160009b327dfa
EmmanuelObasi <authentic.emmy@gmail.com>
2018-05-02 16:28:27 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(1);
-mysql_connect('127.0.0.1', 'azure@localhost', '') or die(mysql_error());
+mysql_connect('127.0.0.1', 'azure@localhost', 'Obasyho@1986') or die(mysql_error());
 mysql_select_db('poll') or die(mysql_error());
 
 ?>
