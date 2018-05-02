c7025a103383b9811b253dd3ec112583b52bfffe
EmmanuelObasi <authentic.emmy@gmail.com>
2018-05-02 16:32:17 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(1);
 mysql_connect('CSDM-WEBDEV', '1509234', '1509234') or die(mysql_error());
-mysql_select_db('poll') or die(mysql_error());
+mysql_select_db('db1509234_poll') or die(mysql_error());
 
 ?>
